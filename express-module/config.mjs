d73362ca341ab5ce917eedef2899963fcb183d06
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-09-13 10:28:50 -0400

    Initial content

---
+++ express-module/config.mjs
@@ -0,0 +1,21 @@
+/**
+ * (#)config.mjs    1.0.0   09/13/2023
+ *
+ * Copyright (c) Jonathan M. Parker
+ * 324 Lantana Drive
+ * Owings Mills, MD  21117 U.S.A
+ * All Rights Reserved.
+ */
+
+/**
+ * The configuration.
+ */
+const config = {
+    htmlContent: {
+        about: '<h1>About Express Module</h1>',
+        root: '<h1>Express Module Home</h1>'
+    },
+    port: 8080
+};
+
+export { config };
